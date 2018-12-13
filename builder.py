e6dd4c17c9a04d86ec1ba4f01dd9b187d10ba1a5
Ms0ng <maplelan15@gmail.com>
2018-12-13 22:48:36 +0800

    test

--- builder.py
+++ builder.py
@@ -1,4 +1,22 @@
 import os,sys
-print(sys.path[0])
-print(os.system("cd ../&& ls"))
-print(os.system("npm install -i -g carbon-now-cli"))
+def main():
+    #配置
+    cwd = sys.path[0]   #当前目录，不带\
+    retry=5
+    surffix="c"
+	print(walk4files(cwd,""))
+
+def walk4files(dir,suffix = ""):
+    nameList=[] 
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            if suffix == "":
+                nameList.append(name)
+            elif re.findall(suffix+"$", name, re.I):
+                nameList.append(name)
+            else:
+                continue
+    return nameList
+	
+if __name__=="__main__":
+main()
